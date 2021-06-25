890549fdc2c1d298f856c6e94c2445594c656eff
Patrik Nilsson <patriknilsson1992@gmail.com>
2021-06-25 12:15:04 +0200

    Adds auth & user

---
+++ database/migrations/1624575281641_users.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('email').notNullable()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
+      table.string('password').notNullable()
+      table.string('remember_me_token').nullable()
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
