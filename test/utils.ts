e8c48ea8e60acc5aed908c82cea26840adc3c33d
Patrik Nilsson <patriknilsson1992@gmail.com>
2021-06-26 12:36:29 +0200

    Adds tests for boards

--- test/utils.ts
+++ test/utils.ts
@@ -1,3 +1,15 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import User from 'App/Models/User'
+import supertest from 'supertest'
+
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-export { BASE_URL }
+async function login() {
+  const { body } = await supertest(BASE_URL)
+    .post('/login')
+    .send({ email: 'johndoe@gmail.com', password: 'test123' })
+
+  return body.token
+}
+
+export { BASE_URL, login }
