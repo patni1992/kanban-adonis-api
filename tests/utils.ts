4905c1fd604576817b2409ba92f7baee0d88c407
Patrik Nilsson <patriknilsson1992@gmail.com>
2022-09-18 21:00:07 +0200

    Updates dependencies

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,4 +1,4 @@
-import { ApiClient, ApiRequest } from '@japa/api-client'
+import { ApiRequest } from '@japa/api-client'
 import User from 'App/Models/User'
 const login = async (request: ApiRequest, userId: number = 1) => {
   const user = await User.find(userId)
